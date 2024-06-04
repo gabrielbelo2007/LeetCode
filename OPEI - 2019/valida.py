f7779a72214094fdefff02408a1f89989f043f28
Gabriel <gabrielbelo2007@gmail.com>
2024-06-04 12:39:17 -0300

    Validação de Cartão - 2019

---
+++ OPEI - 2019/valida.py
@@ -0,0 +1,22 @@
+N = input()
+
+contador = 2
+multiplo_10 = 0
+for valor in N: 
+    if contador % 2 == 0:
+        multiplicacao = int(valor) * 2
+        contador += 1
+        
+        if multiplicacao >= 10:
+            multiplicacao = str(multiplicacao)
+            multiplo_10 += int(multiplicacao[0]) + int(multiplicacao[1])
+        
+        else:
+            multiplo_10 += multiplicacao
+            
+    else:
+        multiplo_10 += int(valor) * 1
+        contador += 1
+
+S = "SIM" if multiplo_10 % 10 == 0 else "NAO"
+print(S)
