5386644fb7f98c3cb948ca36bec203c94fa0bd2a
Gabriel <gabrielbelo2007@gmail.com>
2024-07-02 13:47:58 -0300

    Lista - Adicional

--- OBI/Fase - 2/2021/lista.py
+++ OBI/Fase - 2/2021/lista.py
@@ -4,10 +4,14 @@ lista = [int(numero) for numero in input().split()]
 indice = 0
 indice_final = elementos_lista - 1
 operacoes = 0
+lista_formatada = []
 
 while indice < indice_final:
      
     if lista[indice] == lista[indice_final]:
+        
+        if (indice + 1) < (indice_final - 1):
+            lista_formatada.append(lista[indice])
         
         indice += 1
         indice_final -= 1
@@ -15,6 +19,7 @@ while indice < indice_final:
     elif lista[indice] > lista[indice_final]:
             
         lista[indice_final - 1] += lista[indice_final]
+        lista_formatada.append(lista[indice_final - 1])
         
         indice_final -= 1
         operacoes += 1
@@ -22,8 +27,12 @@ while indice < indice_final:
     elif lista[indice] < lista[indice_final]:
             
         lista[indice + 1] += lista[indice]
+        lista_formatada.append(lista[indice + 1])
 
         indice += 1
         operacoes += 1
-   
+
 print(operacoes)
+
+# Adicional:  
+print(lista_formatada)  
