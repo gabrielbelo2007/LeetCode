e8cd2e4fa15ecd4165c33abfa0b8ad965251ff9a
Gabriel <gabrielbelo2007@gmail.com>
2024-07-02 13:37:33 -0300

    Lista Palíndroma - 100/100

--- OBI/Fase - 2/2021/lista.py
+++ OBI/Fase - 2/2021/lista.py
@@ -1,46 +1,29 @@
-# Somar os numeros um do lado do outro até a lista ser igual de traz para frente
-# 10 60 20 40 10
-# 10 60 -> 20 + 40 <- 10
-# 10 60 60 10
-
 elementos_lista = int(input())
 lista = [int(numero) for numero in input().split()]
-
-operações = 0
-
-def verificar(lista):
-    for i in range(len(lista)):
-        if lista[i] == lista[-(i + 1)]:
-            continue
-        else:
-            return False
-    return True
     
 indice = 0
-indice_final = -1
-while verificar(lista) is False:
-    if lista[indice] != lista[indice_final]:
-        operações += 1
-        
-        if indice < (len(lista) - 1):
-            try:  # ANALISAR
-                if lista[indice] == min(lista[indice] + lista[indice + 1], lista[indice + 1] + lista[indice + 2]):
-                    lista[indice + 1] = min(lista[indice] + lista[indice + 1], lista[indice + 1] + lista[indice + 2])
-                    lista.pop(indice + 2)
-                else:
-                    lista[indice] = lista[indice] + lista[indice + 1]
-                    lista.pop(indice + 1)
+indice_final = elementos_lista - 1
+operacoes = 0
 
-            except:
-                lista[indice] = lista[indice] + lista[indice + 1]
-                lista.pop(indice + 1)
-                
-        else:
-            lista[indice] = lista[indice] + lista[indice - 1]
-            lista.pop(indice - 1)
-            
-    else:
+while indice < indice_final:
+     
+    if lista[indice] == lista[indice_final]:
+        
         indice += 1
         indice_final -= 1
+           
+    elif lista[indice] > lista[indice_final]:
+            
+        lista[indice_final - 1] += lista[indice_final]
+        
+        indice_final -= 1
+        operacoes += 1
+            
+    elif lista[indice] < lista[indice_final]:
+            
+        lista[indice + 1] += lista[indice]
 
-print(operações)
+        indice += 1
+        operacoes += 1
+   
+print(operacoes)
--- teste.py
+++ teste.py
@@ -1,4 +1,32 @@
-lista = [10, 20, 30, 40]
+def min_contractions_to_palindrome(n, lst):
+    # Use dois ponteiros para verificar a lista
+    left = 0
+    right = n - 1
+    operations = 0
 
-teste = min(lista[0] + lista[1], lista[2] + lista[3])
-print(teste)
+    while left < right:
+        if lst[left] == lst[right]:
+            left += 1
+            right -= 1
+            
+        elif lst[left] > lst[right]:
+            lst[right - 1] += lst[right]
+            right -= 1
+            operations += 1
+            
+        elif lst[left] < lst[right]:
+            lst[left + 1] += lst[left]
+            left += 1
+            operations += 1
+
+    return operations
+
+# Leitura dos dados de entrada
+n = int(input())
+lst = list(map(int, input().split()))
+
+# Chamada da função para obter o número mínimo de operações
+result = min_contractions_to_palindrome(n, lst)
+
+# Impressão do resultado
+print(result)
