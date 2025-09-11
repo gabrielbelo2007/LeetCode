635d04f78171d7f807f98f232f27fe9aa162eb07
gabrielbelo2007 <gabrielbelo.dev@gmail.com>
2025-09-11 20:07:59 -0300

    Dia 4 - Group Anagrams & Binary Search

--- GRIND dos fellas/Dia_3/group_anagrams.py
+++ GRIND dos fellas/Dia_3/group_anagrams.py
@@ -2,4 +2,18 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        output = []
+        temp = []
+
+        while (len(strs) > 0):
+            word = strs[0]
+            temp.append(word)
+            for str in strs[1:]:
+                if(len(word) == len(str)):
+                    if(sorted(word) == sorted(str)):
+                        temp.append(str)
+                        strs.remove(str)
+                        
+            strs.remove(word)
+            output.append(temp)
+            temp = []
